65a30ff2bfe88811bd98672bc95b8a7582d668a5
mixail5428 <mix-delo@yandex.ru>
2020-12-21 14:34:16 +0300

    first commit

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 22);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 449);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(197, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 553);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        Thread myThread;
+        DataGridView dgv;
+        double Nx = 5;
+        double Ny = 10;
+        double h = 1;
+        float temp4 = 20;
+        float temp2 = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgv = dataGridView1;
+            int n = (int)(Nx / h);
+            int m = (int)(Ny / h);
+            dgv.ColumnCount = n;
+            dgv.RowCount = m;
+        }
+
+        private void calculation()
+        {
+            const double a = 10;
+            double h = 1;
+            double tau = 0.01;
+            double t0 = 0;
+            double tmax = 5;
+            double R = a  * tau / h;
+            int n = (int)(Nx / h);
+            int m = (int)(Ny / h);
+            double[,] result = new double[m, n];
+            double[,] item = new double[m, n];
+
+            float r = 255 / (temp4 > temp2 ? temp4 + 1: temp2 + 1);
+
+            if (R < 0.5)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        result[i, j] = item[i, j] = 0;
+                        //сторона 3
+                        if (i == 0)
+                            item[i, j] = result[i, j] = 0;
+                        //сторона 1
+                        if (i == m - 1)
+                            item[i, j] = result[i, j] = 0;
+                    }
+                    //сторона 2
+                    item[i, n - 1] = result[i, n - 1] = temp2;
+                    //сторона 4
+                    item[i, 0] = result[i, 0] = temp4;
+                }
+
+                // отрисовка исходных данных
+                for (int i = 0; i < m ; i++)
+                {
+                    for (int j = 0; j < n ; j++)
+                    {
+                        dgv[j, i].Value = item[i, j];
+                        dgv[j, i].Style.BackColor = Color.FromArgb(255, (int)(result[i, j] * r), 255 - (int)(result[i, j] * r), 0);
+                    }
+                }
+
+               // вычисления
+                for (double t = t0 + tau; t <= tmax; t += tau)
+                {
+                    for (int i = 1; i < m - 1; i++)
+                    {
+                        for (int j = 1; j < n - 1; j++)
+                        {
+                            result[i, j] = item[i, j] + R * (item[i + 1, j] + item[i - 1, j] + item[i, j + 1] + item[i, j - 1] - 4 * item[i, j]);
+                            dgv[j, i].Value = String.Format("{0:0.00}", result[i, j]);
+                            dgv[j, i].Style.BackColor = Color.FromArgb(255, (int)(result[i, j] * r), 255 - (int)(result[i, j] * r), 0);
+                        }
+                    }
+                    UpdateData(item, result);
+                    Thread.Sleep(100);
+                    
+                }
+            }
+        }
+
+        private void UpdateData(double[,] A, double[,] B)
+        {
+            for (int i = 0; i < A.GetLength(0); i++)
+                for (int j = 0; j < A.GetLength(1); j++)
+                    A[i, j] = B[i, j];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(myThread != null)
+                myThread.Abort();
+
+            myThread = new Thread(new ThreadStart(calculation));
+            myThread.Start(); // запускаем поток
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(myThread != null)
+                myThread.Abort();
+            myThread = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (myThread != null)
+                myThread.Abort();
+            myThread = null;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            this.dgv.ClearSelection();
+        }
+    }
+}
